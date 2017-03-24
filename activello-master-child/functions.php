92f0f1ec0e257360086a1801545b5e05dbdcf857
Ethan DeGross <ethan-degross@uiowa.edu>
2017-03-24 15:50:43 -0500

    Added random filter to blogroll

--- activello-master-child/functions.php
+++ activello-master-child/functions.php
@@ -19,18 +19,19 @@ function slug_let_admins_admin( $has_access, $cap, $capability ) {
 /*
     Set the Feedzy cache lifetime for a specfic feed
 */
-function bweb_feedzy_cache_duration( $feedCacheDuration, $feedURL ) {
-    if( 'https://dsps.lib.uiowa.edu/atiowa/category/blackatiowa/feed/' == $feedURL )
+function bweb_feedzy_cache_duration( $feedCacheDuration) {
         return 600; // integer in seconds
- 
+
     return $feedCacheDuration;
 }
 add_filter( 'wp_feed_cache_transient_lifetime', 'bweb_feedzy_cache_duration', 10, 2 );
 
-function feedzy_feed_items_random( $items, $feedURL ){
-	if( 'https://dsps.lib.uiowa.edu/atiowa/category/blackatiowa/feed/' == $feedURL ) {
+/*
+    Set the Feedzy posts to random
+*/
+function feedzy_feed_items_random( $items){
 		shuffle ( $items );
-	}
 	return $items;
 }
-add_filter('feedzy_feed_items', 'feedzy_feed_items_random', 9, 2); 
+
+add_filter('feedzy_feed_items', 'feedzy_feed_items_random', 9, 2);
